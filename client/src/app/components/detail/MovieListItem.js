856e1d8f8ab817e37f827a62d2e90b5906c68c32
Silke Van Meerbeek <silkvanm1@student.arteveldehs.be>
2021-06-04 11:25:35 +0200

    movielist

--- client/src/app/components/detail/MovieListItem.js
+++ client/src/app/components/detail/MovieListItem.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 import * as Routes from '../../routes';
 
-import styles from './ProjectListItem.module.scss';
+import styles from './MovieListItem.module.scss';
 
 const MovieListItem = ({ movie }) => {
   return (
-    <article>
+    <article class={styles.movielistItem}>
         <h3 className={styles.title}><Link to={Routes.MOVIE_DETAILS.replace(':id', movie.id)}>{ movie.title }</Link></h3>
         <span className={styles.numViews}><FiFilm /><span>{ project.numViews }</span></span>
     </article>
